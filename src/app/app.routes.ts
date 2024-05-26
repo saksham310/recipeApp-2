a6ed188f5e3a6942ff04d60f19dd05bbda586133
saksham.s <sakshams982@gmail.com>
2024-05-26 11:49:26 +0545

    added a component for detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './pages/card/card.component';
 import { DisplayComponent } from './pages/display/display.component';
 import { RecipeFormComponent } from './form/recipe-form/recipe-form.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 export const routes: Routes = [
     {
         path:'',component:HomeComponent,title:"Welcome Page"
     },
     { path:'cards',component:DisplayComponent,title:"Recipe Lists"
-},    { path:'form/:id',component:RecipeFormComponent,title:"Recipe Form"
+},    { path:'form/:id',component:RecipeFormComponent,title:"Edit-Recipe Form"
 },    { path:'form',component:RecipeFormComponent,title:"Recipe Form"
+},{ path:'detail/:id',component:DetailComponent,title:"Detail Page"
 }
     
 ];
