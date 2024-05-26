3a669c1f40fdd39ee2f2f240ff2559db633c0cd7
saksham.s <sakshams982@gmail.com>
2024-05-26 14:59:57 +0545

    added a button for recipe creation page

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,21 +1,28 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FoodService } from '../../serivce/food.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FontAwesomeModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   @ViewChild("search")
   search !:ElementRef;
-  constructor(private service:FoodService){}
+  plusBtn=faPlusCircle;
+  constructor(private service:FoodService,private route:Router){}
 
 onSearch(){
 // console.log(this.search.nativeElement.value);
 this.service.onSearch(this.search.nativeElement.value);
+}
+isCreation(){
+  return this.route.url==="/form"
 }
 }
